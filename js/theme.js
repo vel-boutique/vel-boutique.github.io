cd10ab21cda80c9d15d15c19fdcc82af7f26e326
antonio-davis <72887796+antonio-davis@users.noreply.github.com>
2021-01-07 17:27:38 +0100

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,5 @@
 function background_shading2() {
-    var scale = chroma.scale(['#181818', '#8a59bb']).domain([0, $(document).height()]);
+    var scale = chroma.scale(['#000000', '#7b1fa2']).domain([0, $(document).height()]);
     $(window).on('scroll', function () {
         $('.body-home').css('background-color', scale(window.pageYOffset));
         console.log("Bella li bro")
