8f344847de42c2c3cb274c1b62b1dc4df4460697
simone <simonepandolfi@protonmail.com>
2021-01-07 16:22:43 +0100

    drill

--- js/theme.js
+++ js/theme.js
@@ -33,3 +33,11 @@ function background_shading() {
         }
     })
 }
+
+function background_shading2() {
+    var scale = chroma.scale(['#181818', '#8a59bb']).domain([0, $(document).height()]);
+    $(window).on('scroll', function () {
+        $('.scroll-container').css('background-color', scale(window.pageYOffset));   
+        console.log("Bella li bro")
+    });
+}
