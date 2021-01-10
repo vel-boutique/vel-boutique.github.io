4dfd965aa9409db7c31f4dfbb752e22750145253
simone <simonepandolfi@protonmail.com>
2021-01-10 17:10:23 +0100

    777

--- js/theme.js
+++ js/theme.js
@@ -2,11 +2,6 @@ function background_shading2() {
     var scale = chroma.scale(['#000000', '#7b1fa2']).domain([0, $(document).height()]);
     $(window).on('scroll', function () {
         $('.body-home').css('background-color', scale(window.pageYOffset));
-        console.log("Bella li bro")
     });
 }
 
-function scroll_to_shop() {
-    var title = document.getElementById("slide-shop");
-    title.scrollIntoView({ behavior: 'smooth' });
-}
