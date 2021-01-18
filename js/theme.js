2de879fc9e3cdcf5c75e9802754fd47a39d31cbd
simone <simonepandolfi@protonmail.com>
2021-01-18 13:54:23 +0100

    www

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,6 @@
-function background_shading2() {
+function background_shading() {
     var scale = chroma.scale(['#000000', '#7b1fa2']).domain([0, $(document).height()]);
     $(window).on('scroll', function () {
         $('.body-home').css('background-color', scale(window.pageYOffset));
     });
 }
-
