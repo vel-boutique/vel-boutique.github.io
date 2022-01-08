899f0ac185bde7f6d65d507514a74f82fdd01649
Simone <simonepandolfi@protonmail.com>
2022-01-08 15:59:38 +0100

    push to check

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,5 @@
+// la teniamo, non si sa mai
+
 function background_shading() {
     var scale = chroma.scale(['#000000', '#7b1fa2']).domain([0, $(document).height()]);
     $(window).on('scroll', function () {
